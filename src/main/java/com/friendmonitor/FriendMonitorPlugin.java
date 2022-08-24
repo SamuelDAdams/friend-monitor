8c91adb244c3e59ead08dd12312ec6295460d168
Samuel Adams <samuel.adams.d@gmail.com>
2022-08-23 17:51:24 -0700

    Additional imports

--- src/main/java/com/friendmonitor/FriendMonitorPlugin.java
+++ src/main/java/com/friendmonitor/FriendMonitorPlugin.java
@@ -2,11 +2,16 @@ package com.friendmonitor;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
+import net.runelite.api.*;
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
+import net.runelite.api.events.StatChanged;
+import net.runelite.api.events.VarbitChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
